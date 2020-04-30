29a3f902ff6f5d47aca1212887842996f61f868b
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-04-30 15:48:33 +0100

    Modified the file

--- 30daysofcode/day1.py
+++ 30daysofcode/day1.py
@@ -1,7 +1,14 @@
 def is_perfect_square(number):
+    '''
+    A function that takes in a parameter number and checks if it is a prime number.
+    Returns:True if perfect square and False not. 
+    '''
     for i in range(number//2):
         if i * i == number:
             return True
     return False
 
 print(is_perfect_square(9))
+print(is_perfect_square(100))
+print(is_perfect_square(225))
+print(is_perfect_square(500))
