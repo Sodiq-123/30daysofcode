6b8a7e29002b16af71ddc16d090b00dda07cb8d7
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-04-30 22:49:10 +0100

    Added a doc string

--- 30daysofcode/day3.py
+++ 30daysofcode/day3.py
@@ -1,9 +1,18 @@
 def is_even(num):
+    '''
+    A function that checks if a given number is even.
+    Returns True if the number is even and false if otherwise.
+    '''
     if num % 2 == 0:
         return True
     return False
 
 def even_odd(list_num):
+    '''
+    A function that uses the function is_even to filter all the even numbers 
+    in a list and also gets the number of odd numbers in the list and also the finds the number of even numbers in the
+    list and prints them out 
+    '''
     even = [i for i in list_num if is_even(i)]
     return(f'number of even numbers is {len(even)}\nnumber of odd numbers is {len(list_num) - len(even)}')
 
