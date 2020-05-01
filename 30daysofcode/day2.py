672f617a884854409a1fc85b5084c430d94dc014
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-01 20:21:57 +0100

    Added a doc string

--- 30daysofcode/day2.py
+++ 30daysofcode/day2.py
@@ -2,6 +2,7 @@ def missing_numbers(num_list):
     '''
     A function that takes in a parameter num_list and checks for the missing numbers in the list
     according to preference.
+    
     Returns: a sorted list of the missing values of num_list
     '''
     original_list = [x for x in range(num_list[0], num_list[-1] + 1)]
