87e6996464baaaa57a9a8f0d25617f06d6540f32
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-01 21:46:11 +0100

    Added a doc-string

--- 30daysofcode/day5.py
+++ 30daysofcode/day5.py
@@ -2,7 +2,7 @@ def unique(L):
     '''
     A function that takes  in a parameter L which is a list.
 
-    Returns a list of the unique list of the original list
+    Returns a list of unique numbers of the original list
     '''
     return f'Sample List: {L}\nUnique List: {sorted(list(set(L)))}'
 
