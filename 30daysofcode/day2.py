06c00dbffeba0d1a97a8173f5111f0b93023a709
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-02 22:47:27 +0100

    Modified the file

--- 30daysofcode/day2.py
+++ 30daysofcode/day2.py
@@ -5,8 +5,8 @@ def missing_numbers(num_list):
 
     Returns: a sorted list of the missing values of num_list
     '''
-    original_list = [x for x in range(num_list[0], num_list[-1] + 1)]
-    return sorted(list(set(original_list)-set(num_list)))
+    missing_values = [x for x in range(min(num_list), max(num_list) + 1) if x not in num_list]
+    return missing_values
 
 print(missing_numbers([1,2,3,4,6,7,10])) #[5, 8, 9]
 print(missing_numbers([10,11,12,14,17])) #[13, 15, 16]
