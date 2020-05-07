7d412d187f977be7e7a04ff07a955bb06d004f90
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-07 07:49:08 +0100

    Added test-cases and time functions

--- 30daysofcode/day8.py
+++ 30daysofcode/day8.py
@@ -7,3 +7,11 @@ def fastSum(n):
 
 print(fastSum(1_000)) #500500
 print(fastSum(1_000_000_000)) #500000000500000000
+print(fastSum(50_505_050))
+print(fastSum(999_999_999_999_999))
+print(fastSum(8_192_892_819_891_112_282_728_282))
+import time
+a = time.time()
+fastSum(8_192_892_819_891_112_282_728_282)
+a = time.time() - a
+print(a)
