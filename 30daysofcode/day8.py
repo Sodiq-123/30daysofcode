2f3a90109c586e3316c431a2218d6cb11a3e2815
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-07 21:54:29 +0100

    Added test-cases and time functions for complexity

--- 30daysofcode/day8.py
+++ 30daysofcode/day8.py
@@ -10,8 +10,13 @@ print(fastSum(1_000_000_000)) #500000000500000000
 print(fastSum(50_505_050)) #1275380063003775
 print(fastSum(999_999_999_999_999)) #499999999999999500000000000000
 print(fastSum(8_192_892_819_891_112_282_728_282)) #33561746379111670802984283972667972742780862699903
-import time
-a = time.time()
-fastSum(8_192_892_819_891_112_282_728_282)
-a = time.time() - a
-print(a) #2.86102294921875e-06
+
+def time_checker(fastSum):
+    import time
+    a = time.time()
+    fastSum(8_192_892_819_891_112_282_728_282)
+    a = time.time() - a
+    return a 
+
+print(time_checker(fastSum))
+# print(time_checker(fastSum)<4.000000000000e-06) == True
