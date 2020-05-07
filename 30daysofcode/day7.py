3ed87803ca6a0735e1ebee73709ade59b4bc8a2e
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-07 06:49:00 +0100

    Added Doc-strings

--- 30daysofcode/day7.py
+++ 30daysofcode/day7.py
@@ -1,6 +1,8 @@
 def averageMultiple(num):
     '''
-    
+    A function that takes in a parameter num and checks all the values from 
+    0 till the number if it's a multiple of 3 or 5
+    Returns: the average of the numbers generated 
     '''
     x = [i for i in range(1, num) if i % 3 == 0 or i % 5 == 0]
     return sum(x)/len(x)
