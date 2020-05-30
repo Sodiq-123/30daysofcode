f93259a398dd883bfb7cdd8053e9db43a8d608d5
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-05-30 10:02:07 +0100

    Modified the code

--- 30daysofcode/day29.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums, target):
-        lookup = {}
-        for i, num in enumerate(nums):
-            if target - num in lookup:
-                return (lookup[target - num], i)
-            lookup[num] = i
-        return lookup
-
-print(twoSum((10,20,10,40,50,60,70), 50))
